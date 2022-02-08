d0ec7db99c723909b348687d934ca5834a9e3e9c
guobao.sun <guobao.sun@ecovacs.com>
2022-02-08 10:30:56 +0800

    modify UiMsgInterceptor

--- bandage/src/main/java/com/panda912/bandage/hook/activity_thread_hook/processors/UiMsgInterceptor.kt
+++ bandage/src/main/java/com/panda912/bandage/hook/activity_thread_hook/processors/UiMsgInterceptor.kt
@@ -2,9 +2,11 @@ package com.panda912.bandage.hook.activity_thread_hook.processors
 
 import android.os.Build
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
 import com.android.internal.os.SomeArgs
 import com.panda912.bandage.hook.activity_thread_hook.Processor
 import com.panda912.bandage.logger.BandageLogger
+import java.io.IOException
 
 /**
  * Created by panda on 2021/12/7 10:25
@@ -23,11 +25,16 @@ class UiMsgInterceptor : Processor {
 
     if (message.obj is SomeArgs) {
       val someArgs = message.obj as SomeArgs
-      if (someArgs.arg2 is Bundle && (someArgs.arg2 as Bundle).containsKey("IGrabNodeReceiver")) {
-        BandageLogger.i(TAG, "huawei is grabing node.")
-        return true
+      if (someArgs.arg2 !is Bundle) {
+        return chain.proceed(message)
       }
-      return chain.proceed(message)
+      val bundle = someArgs.arg2 as? Bundle
+      if (bundle?.containsKey("IGrabNodeReceiver") != true) {
+        return chain.proceed(message)
+      }
+      BandageLogger.i(TAG, "huawei is grabing node.")
+      closeFD(bundle)
+      return true
     } else if (message.obj.javaClass.name != "android.app.ActivityThread\$RequestContentNode") {
       return chain.proceed(message)
     } else {
@@ -36,6 +43,19 @@ class UiMsgInterceptor : Processor {
       BandageLogger.i(TAG, "huawei is grabing node.")
       return true
     }
-
   }
+
+  private fun closeFD(bundle: Bundle?) {
+    if (bundle?.containsKey("read_file_descripter") == true) {
+      val obj = bundle.get("read_file_descripter")
+      if (obj is ParcelFileDescriptor) {
+        try {
+          obj.close()
+        } catch (e: IOException) {
+          BandageLogger.w(TAG, "close fd exception: ${e.message}")
+        }
+      }
+    }
+  }
+
 }
