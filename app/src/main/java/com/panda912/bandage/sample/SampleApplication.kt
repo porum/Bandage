ec44994db947ff44a79287b4f65dd773f7a29499
porum <sunguobao12@gmail.com>
2022-02-09 13:39:21 +0800

    modify sample

--- app/src/main/java/com/panda912/bandage/sample/SampleApplication.kt
+++ app/src/main/java/com/panda912/bandage/sample/SampleApplication.kt
@@ -30,18 +30,14 @@ class SampleApplication : Application() {
     val stack1 =
       "        at com.panda912.bandage.sample.MainActivity.onCreate\$lambda-1(MainActivity.kt:20)\n" +
           "        at com.panda912.bandage.sample.MainActivity.\$r8\$lambda\$rFwK1IpRD1jDgZ5A7zMkoEr2cEs(Unknown Source:0)\n" +
-          "        at com.panda912.bandage.sample.MainActivity\$\$ExternalSyntheticLambda2.onClick(Unknown Source:0)\n" +
+          "        at com.panda912.bandage.sample.MainActivity\$\$ExternalSyntheticLambda3.onClick(Unknown Source:0)\n" +
           "        at android.view.View.performClick(View.java:7575)\n" +
           "        at android.view.View.performClickInternal(View.java:7548)\n" +
           "        at android.view.View.access\$3600(View.java:837)\n" +
           "        at android.view.View\$PerformClick.run(View.java:28902)\n" +
           "        at android.os.Handler.handleCallback(Handler.java:938)\n" +
           "        at android.os.Handler.dispatchMessage(Handler.java:99)\n" +
-          "        at android.os.Looper.loop(Looper.java:236)\n" +
-          "        at android.app.ActivityThread.main(ActivityThread.java:8071)\n" +
-          "        at java.lang.reflect.Method.invoke(Native Method)\n" +
-          "        at com.android.internal.os.RuntimeInit\$MethodAndArgsCaller.run(RuntimeInit.java:620)\n" +
-          "        at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:1011)"
+          "        at android.os.Looper.loop(Looper.java:236)"
     val data1 = DynamicBandageData(
       process = "all",
       exceptionMatch = DynamicBandageData.ExceptionMatch("java.lang.ArithmeticException", ""),
