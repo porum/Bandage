087463ed7be4313795258a1ceae2b2e3339941b2
porum <sunguobao12@gmail.com>
2023-08-12 19:32:37 +0800

    update sample

--- app/src/main/java/com/panda912/bandage/sample/MainActivity.kt
+++ app/src/main/java/com/panda912/bandage/sample/MainActivity.kt
@@ -1,33 +1,16 @@
 package com.panda912.bandage.sample
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import kotlin.concurrent.thread
 
 class MainActivity : Activity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-
-    findViewById<Button>(R.id.btn).setOnClickListener {
-      startActivity(Intent(this, DetailActivity::class.java))
-    }
-
-    findViewById<Button>(R.id.btn2).setOnClickListener {
+    findViewById<Button>(R.id.button).setOnClickListener {
       println(10 / 0)
-    }
-
-    findViewById<Button>(R.id.btn3).setOnClickListener {
-      throw Exception("aoh")
-    }
-
-    findViewById<Button>(R.id.btn4).setOnClickListener {
-      thread {
-        throw RuntimeException("crash in sub thread.")
-      }
     }
   }
 }
--- app/src/main/java/com/panda912/bandage/sample/SampleApplication.kt
+++ app/src/main/java/com/panda912/bandage/sample/SampleApplication.kt
@@ -25,43 +25,20 @@ class SampleApplication : Application() {
 
   private fun createDynamicBandageData() {
     // click button 'crash 10 / 0'
-    val stack1 =
-      "        at com.panda912.bandage.sample.MainActivity.onCreate\$lambda-1(MainActivity.kt:20)\n" +
-          "        at com.panda912.bandage.sample.MainActivity.\$r8\$lambda\$rFwK1IpRD1jDgZ5A7zMkoEr2cEs(Unknown Source:0)\n" +
-          "        at com.panda912.bandage.sample.MainActivity\$\$ExternalSyntheticLambda3.onClick(Unknown Source:0)\n" +
-          "        at android.view.View.performClick(View.java:7575)\n" +
-          "        at android.view.View.performClickInternal(View.java:7548)\n" +
-          "        at android.view.View.access\$3600(View.java:837)\n" +
-          "        at android.view.View\$PerformClick.run(View.java:28902)\n" +
-          "        at android.os.Handler.handleCallback(Handler.java:938)\n" +
-          "        at android.os.Handler.dispatchMessage(Handler.java:99)\n" +
-          "        at android.os.Looper.loop(Looper.java:236)"
-    val data1 = DynamicBandageData(
+    val stack =
+      "at com.panda912.bandage.sample.MainActivity.onCreate\$lambda-0(MainActivity.kt:14)\n" +
+          "at com.panda912.bandage.sample.MainActivity.\$r8\$lambda\$DDlE6wDlIjCyOonj-NgrVOem29Q(Unknown Source:0)\n" +
+          "at com.panda912.bandage.sample.MainActivity\$\$ExternalSyntheticLambda0.onClick(Unknown Source:0)\n" +
+          "at android.view.View.performClick(View.java:7506)"
+    val data = DynamicBandageData(
       process = "all",
       exceptionMatch = DynamicBandageData.ExceptionMatch("java.lang.ArithmeticException", ""),
-      stacks = stack1.lines().map { it.trim().replace(Regex("(at |\\([^\\)]*\\))"), "") },
+      stacks = stack.lines().map { it.trim().replace(Regex("(at |\\([^\\)]*\\))"), "") },
       causes = null,
       closeCurActivity = false,
       loadPatch = false,
     )
 
-    // click button 'crash in sub thread'
-    val stack2 =
-      "        at com.panda912.bandage.sample.MainActivity\$onCreate\$4\$1.invoke(MainActivity.kt:29)\n" +
-          "        at com.panda912.bandage.sample.MainActivity\$onCreate\$4\$1.invoke(MainActivity.kt:28)\n" +
-          "        at kotlin.concurrent.ThreadsKt\$thread\$thread\$1.run(Thread.kt:30)"
-    val data2 = DynamicBandageData(
-      process = "all",
-      exceptionMatch = DynamicBandageData.ExceptionMatch(
-        "java.lang.RuntimeException",
-        "crash in sub thread."
-      ),
-      stacks = stack2.lines().map { it.trim().replace(Regex("(at |\\([^\\)]*\\))"), "") },
-      causes = null,
-      closeCurActivity = false,
-      loadPatch = false,
-    )
-
-    Bandage.addDynamicBandageData(listOf(data1, data2))
+    Bandage.addDynamicBandageData(listOf(data))
   }
 }
